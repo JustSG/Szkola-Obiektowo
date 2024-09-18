0e0eb517b0a91635f940819d16cc0033ad261577
JustSG <szymionfujawa.07@gmail.com>
2024-09-18 14:31:47 +0200

    wypisywanie imienia i konstruktor

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Osoba osobaJas = new Osoba("Jaś", "Kowalski", 12);
+        System.out.println("imie: " + osobaJas.imie);
+    }
+}
